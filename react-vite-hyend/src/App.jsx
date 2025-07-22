492e8b84bd3345757539e7e4c28050d123ff5db2
jaelllim <github jaelim0429@hanyang.ac.kr>
2025-07-22 16:29:45 +0900

    style: 배경색 연노랑으로 설정

--- react-vite-hyend/src/App.jsx
+++ react-vite-hyend/src/App.jsx
@@ -115,3 +115,9 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
+[
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, 'X', null, null, null, null],
+  [null, null, null, null,'X', null, null, null, 'O'],
+]
