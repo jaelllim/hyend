8d1137fc08838be2ef5c145e5705692d1bae8753
jaelllim <github jaelim0429@hanyang.ac.kr>
2025-07-25 23:54:26 +0900

    feat: 체크박스 컴포넌트 구현 및 스타일 추가

---
+++ tstodolist/src/App.tsx
@@ -0,0 +1,17 @@
+//App.tsx
+
+import React from 'react';
+import './App.css';
+const App = () => {
+  return (
+    <>
+      <div className="mainContainer">
+        <div className="appContent">
+          <input type="text" className="inputText" placeholder="내용을 입력 후 엔터" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
